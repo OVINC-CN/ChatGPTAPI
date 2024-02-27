01c448ab66891c83b43283af8732d37034eed51c
orenzhang <orenzhang@ovinc.cn>
2024-02-27 14:54:38 +0800

    fix: worker oom

--- apps/cel/tasks/chat.py
+++ apps/cel/tasks/chat.py
@@ -1,5 +1,6 @@
 from django.db import transaction
 from django.db.models import F, Func, Value
+from django.shortcuts import get_object_or_404
 from ovinc_client.core.lock import task_lock
 
 from apps.cel import app
@@ -14,17 +15,28 @@ def check_usage_limit(self):
     """
 
     # load logs
-    logs = ChatLog.objects.filter(is_charged=False)
+    logs = ChatLog.objects.filter(is_charged=False, finished_at__isnull=False).values_list("id", flat=True)
 
     # charge
-    for log in logs:
-        usage = log.prompt_tokens + log.completion_tokens
-        with transaction.atomic():
-            ChatLog.objects.filter(id=log.id).update(is_charged=True)
-            ModelPermission.objects.filter(user=log.user, model=log.model).update(
-                available_usage=Func(
-                    F("available_usage") - usage,
-                    Value(0),
-                    function="GREATEST",
-                )
-            )
+    for log_id in logs:
+        calculate_usage_limit.apply_async(kwargs={"log_id": log_id})
+
+
+@app.task(bind=True)
+@transaction.atomic()
+def calculate_usage_limit(self, log_id: str):
+    """
+    Calculate Model Usage Limit
+    """
+
+    log = get_object_or_404(ChatLog, id=log_id)
+
+    usage = log.prompt_tokens + log.completion_tokens
+    ChatLog.objects.filter(id=log.id).update(is_charged=True)
+    ModelPermission.objects.filter(user=log.user, model=log.model).update(
+        available_usage=Func(
+            F("available_usage") - usage,
+            Value(0),
+            function="GREATEST",
+        )
+    )
