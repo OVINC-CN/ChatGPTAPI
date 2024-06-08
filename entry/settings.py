fc368ccd034b8f53a53233e2dca074b0aac2a8da
Oren Zhang <orenzhang@ovinc.cn>
2024-06-09 03:46:52 +0800

    fix: channel layer cannot be none

--- entry/settings.py
+++ entry/settings.py
@@ -115,6 +115,16 @@ CACHES = {
 
 # ASGI
 ASGI_APPLICATION = "entry.asgi.application"
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [
+                f"redis://:{REDIS_PASSWORD}@{REDIS_HOST}:{REDIS_PORT}/{REDIS_DB}",
+            ],
+        },
+    },
+}
 
 # Auth
 AUTH_PASSWORD_VALIDATORS = [
