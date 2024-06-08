450dd91e86704cb212e0b5619b31080f99af4b27
Oren Zhang <orenzhang@ovinc.cn>
2024-06-08 15:57:07 +0800

    chore: update startup script

--- bin/run.sh
+++ bin/run.sh
@@ -1,8 +1,7 @@
 #! /bin/sh
 
-mv logs/* celery-logs
 python manage.py collectstatic --noinput
 python manage.py migrate --noinput
-# nohup python manage.py celery worker -l INFO -f /usr/src/app/celery-logs/worker.log >/dev/null 2>&1 &
-# nohup python manage.py celery beat -l INFO -f /usr/src/app/celery-logs/beat.log >/dev/null 2>&1 &
-uwsgi --ini /usr/src/app/uwsgi.ini --processes $UWSGI_PROCESSES --threads $UWSGI_THREADS -w wsgi.wsgi:application >/dev/null 2>&1
+nohup python manage.py celery worker -c 4 -l INFO >/dev/stdout 2>&1 &
+nohup python manage.py celery beat -l INFO >/dev/stdout 2>&1 &
+daphne -b 0.0.0.0 -p 8020 --websocket_timeout 3600 --access-log daphne-logs/access.log --proxy-headers entry.asgi:application
