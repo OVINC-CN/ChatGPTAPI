f4b9c4362b1f4415d4890a6f3055de6140a39ecd
Oren Zhang <orenzhang@ovinc.cn>
2024-07-01 19:17:25 +0800

    feat: remove debug task

--- apps/cel/tasks/__init__.py
+++ apps/cel/tasks/__init__.py
@@ -1,9 +1,7 @@
 from apps.cel.tasks.chat import check_usage_limit
 from apps.cel.tasks.cos import extract_file
-from apps.cel.tasks.debug import celery_debug
 
 __all__ = [
-    "celery_debug",
     "check_usage_limit",
     "extract_file",
 ]
