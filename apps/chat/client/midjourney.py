40b10605e94b43baea04b0fa5f0b5b79d5610af9
orenzhang <orenzhang@ovinc.cn>
2024-12-18 21:03:27 +0800

    feat(client): use model settings for midjourney instead of env

--- apps/chat/client/midjourney.py
+++ apps/chat/client/midjourney.py
@@ -2,7 +2,6 @@ import asyncio
 import time
 import uuid
 
-from django.conf import settings
 from httpx import AsyncClient
 from opentelemetry.trace import SpanKind
 from ovinc_client.core.logger import logger
@@ -23,28 +22,28 @@ class MidjourneyClient(BaseClient):
     async def _chat(self, *args, **kwargs) -> any:
         client = AsyncClient(
             http2=True,
-            headers={"Authorization": f"Bearer {settings.MIDJOURNEY_API_KEY}"},
-            base_url=settings.MIDJOURNEY_API_BASE_URL,
-            proxy=settings.OPENAI_HTTP_PROXY_URL or None,
-            timeout=settings.MIDJOURNEY_API_TIMEOUT,
+            headers={"Authorization": f"Bearer {self.model_settings.get("api_key")}"},
+            base_url=self.model_settings.get("base_url"),
+            proxy=self.model_settings.get("proxy"),
+            timeout=self.model_settings.get("timeout"),
         )
         # call midjourney api
         try:
             with self.start_span(SpanType.API, SpanKind.CLIENT):
                 # submit job
                 response = await client.post(
-                    url=settings.MIDJOURNEY_IMAGINE_API_PATH, json={"prompt": self.messages[-1].content}
+                    url=self.model_settings.get("imaging_path"), json={"prompt": self.messages[-1].content}
                 )
             result_id = response.json()["result"]
             # wait for result
             start_time = time.time()
-            while time.time() - start_time < settings.MIDJOURNEY_IMAGE_JOB_TIMEOUT:
-                result = await client.get(url=settings.MIDJOURNEY_TASK_RESULT_API_PATH.format(id=result_id))
+            while time.time() - start_time < self.model_settings.get("wait_timeout", 600):
+                result = await client.get(url=self.model_settings.get("result_path").format(id=result_id))
                 result_data = result.json()
                 # if not finished, continue loop
                 if result_data["status"] not in [MidjourneyResult.FAILURE, MidjourneyResult.SUCCESS]:
                     yield ""
-                    await asyncio.sleep(settings.MIDJOURNEY_IMAGE_JOB_INTERVAL)
+                    await asyncio.sleep(self.model_settings.get("no_result_sleep", 5))
                     continue
                 # if failed
                 if result_data["status"] == MidjourneyResult.FAILURE:
