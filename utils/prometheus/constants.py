e2e0758dd6be0a94b6d74905f37e8d0abf1f80c5
orenzhang <orenzhang@ovinc.cn>
2025-01-24 14:42:32 +0800

    feat(chat): use http process for chat progress instead of celery worker

--- utils/prometheus/constants.py
+++ utils/prometheus/constants.py
@@ -1,5 +1,4 @@
 class PrometheusMetrics:
-    WAIT_FOR_PROCESS = "wait_process"
     WAIT_FIRST_LETTER = "first_letter"
     TOKEN_PER_SECOND = "token_per_second"
     WEBSOCKET_CONN = "websocket_conn"
