15938c5a8f1da22a52d0ee17bf894dce1ca81850
orenzhang <orenzhang@ovinc.cn>
2025-02-07 21:05:05 +0800

    feat(chat): remove midjourney support

--- apps/chat/admin.py
+++ apps/chat/admin.py
@@ -89,10 +89,9 @@ class AIModelAdmin(admin.ModelAdmin):
         "request_price",
         "support_vision",
         "support_system_define",
-        "is_vision",
         "is_public",
     ]
-    list_filter = ["provider", "is_enabled", "support_vision", "support_system_define", "is_vision", "is_public"]
+    list_filter = ["provider", "is_enabled", "support_vision", "support_system_define", "is_public"]
     search_fields = ["name"]
 
     @admin.display(description=gettext_lazy("Icon"))
